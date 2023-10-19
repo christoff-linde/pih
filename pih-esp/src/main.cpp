16b59c3f1b133d45f8102dd6e1c22ccdf5f25b82
christoff-linde <christoff.linde@gmail.com>
2023-10-19 16:47:32 +0200

    feat: basic temp test setup

--- pih-esp/src/main.cpp
+++ pih-esp/src/main.cpp
@@ -1,18 +1,56 @@
-#include <Arduino.h>
+// This example gets individual Sensor objects for temperature/pressure!
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_DPS310.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_DPS310 dps;
+Adafruit_Sensor *dps_temp = dps.getTemperatureSensor();
+Adafruit_Sensor *dps_pressure = dps.getPressureSensor();
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  Serial.println("DPS310");
+  if (!dps.begin_I2C())
+  {
+    Serial.println("Failed to find DPS");
+    while (1)
+      yield();
+  }
+  Serial.println("DPS OK!");
+
+  // Setup highest precision
+  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
+
+  dps_temp->printSensorDetails();
+  dps_pressure->printSensorDetails();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  sensors_event_t temp_event, pressure_event;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (dps.temperatureAvailable())
+  {
+    dps_temp->getEvent(&temp_event);
+    Serial.print(F("Temperature = "));
+    Serial.print(temp_event.temperature);
+    Serial.println(" *C");
+    Serial.println();
+  }
+
+  // Reading pressure also reads temp so don't check pressure
+  // before temp!
+  if (dps.pressureAvailable())
+  {
+    dps_pressure->getEvent(&pressure_event);
+    Serial.print(F("Pressure = "));
+    Serial.print(pressure_event.pressure);
+    Serial.println(" hPa");
+
+    Serial.println();
+  }
 }
