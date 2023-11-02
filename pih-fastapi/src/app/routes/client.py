c7ffc09131bfaa6bc10fb1842aefb55fdaf60cd3
christoff-linde <christoff.linde@gmail.com>
2023-11-02 21:15:37 +0200

    feat: add get_all_data endpoint and utils

--- pih-fastapi/src/app/routes/client.py
+++ pih-fastapi/src/app/routes/client.py
@@ -1,11 +1,15 @@
 """Client router module for all client API routes."""
 
-from fastapi import APIRouter
-from datetime import datetime
 import csv
+import json
+import logging
+from datetime import datetime
+from typing import Any, Dict, Hashable, List
+from datetime import datetime
+import pandas as pd
+from fastapi import APIRouter
 
-from app.utils import dump_to_file
-
+logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/api/client/v1", tags=["client"])
 
 
@@ -18,12 +22,47 @@ def update_sensor(sensor_id: str, temperature: float, humidity: float):
         "data": {"temperature": temperature, "humidity": humidity},
     }
 
-    # target_file_name = f"data/update-sensor-{sensor_id}-{now}.json"
-    # dump_to_file(data_obj, target_file_name)
-
     data_row = [now, sensor_id, temperature, humidity]
-    with open("data/data.csv", "a") as csv_file:
+    with open("data/data_home.csv", "a") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(data_row)
 
     return data_obj
+
+
+def get_data(file_path: str = "data/data_home.csv"):
+    df = pd.read_csv(file_path)
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
+    return df.to_dict(orient="records")
+
+
+def transform_data(data):
+    return [
+        {
+            "id": "temperature",
+            "data": [{"x": x["timestamp"], "y": x["temperature"]} for x in data],
+        },
+        {
+            "id": "humidity",
+            "data": [{"x": x["timestamp"], "y": x["humidity"]} for x in data],
+        },
+    ]
+
+
+@router.get("/all-data")
+async def get_all_data(
+    file_path: str = "data/data_home.csv",
+):
+    return get_data(file_path)
+    # return transform_data(data)
+
+    data = {}
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        logger.exception(f"File not found at {file_path}")
+    except Exception as exc:
+        logger.exception(exc)
+
+    return data
